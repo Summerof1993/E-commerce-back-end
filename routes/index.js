527cb4afefa17ed38b9cb69c6c63621d613d2eb2
Springof1993 <alejandrorodriguez309@gmail.com>
2024-02-20 22:47:21 -0800

    created file organization

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+ const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+// what does this block do?
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>")
+});
+
+module.exports = router;

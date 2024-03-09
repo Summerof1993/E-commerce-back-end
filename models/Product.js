173be5ffc8ce30389becaa5c7d22f321cb011c5e
Springof1993 <alejandrorodriguez309@gmail.com>
2024-03-09 10:06:45 -0800

    added routes

--- models/Product.js
+++ models/Product.js
@@ -3,13 +3,48 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize Product model (table) by extending Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
